8ba7287984c18593e3ea2fc0aa7e854a7a875071
Aryan Chaubal <aryanchaubal@icloud.com>
2019-09-11 18:53:03 +0530

    Fixed search related issues

--- CMS-iOS/View Controllers/Dashboard Tab/DashboardViewController.swift
+++ CMS-iOS/View Controllers/Dashboard Tab/DashboardViewController.swift
@@ -20,26 +20,27 @@ class DashboardViewController : UIViewController, UITableViewDelegate, UITableVi
     
     
     
+    
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
     let constant = Constants.Global.self
     var courseList = [Course]()
-    var filteredCourseList = [Course]()
     var userDetails = User()
     var selectedCourseId : Int = 0
     var selectedCourseName : String = ""
-    let refreshControl = UIRefreshControl()
     var searching : Bool = false
+    let refreshControl = UIRefreshControl()
+    var filteredCourseList = [Course]()
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         searchBar.delegate = self
         
+        
         searchBar.showsCancelButton = false
-        searchBar.resignFirstResponder()
+        
         
         refreshControl.tintColor = .black
         refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
@@ -63,12 +64,15 @@ class DashboardViewController : UIViewController, UITableViewDelegate, UITableVi
     
     override func viewDidAppear(_ animated: Bool) {
         SVProgressHUD.dismiss()
-        if let cancelButton = searchBar.value(forKey: "cancelButton") as? UIButton{
-            cancelButton.isEnabled = true
+        if !searching{
+            getRegisteredCourses {
+                
+            }
         }
-        getRegisteredCourses {
-            
-        }
+        
+        tableView.reloadData()
+
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -191,12 +195,17 @@ class DashboardViewController : UIViewController, UITableViewDelegate, UITableVi
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "reuseCell")
-        if searching{
+        
+        
+        if searching {
             cell.textLabel?.text = filteredCourseList[indexPath.row].displayname
+            
         }else{
             cell.textLabel?.text = courseList[indexPath.row].displayname
             
         }
+        
+        
         
         
         return cell
@@ -204,57 +213,54 @@ class DashboardViewController : UIViewController, UITableViewDelegate, UITableVi
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        if !searching{
-        self.selectedCourseId = courseList[indexPath.row].courseid
-        self.selectedCourseName = courseList[indexPath.row].displayname
-        }else{
+        if searching {
             self.selectedCourseId = filteredCourseList[indexPath.row].courseid
             self.selectedCourseName = filteredCourseList[indexPath.row].displayname
+        }else{
+            self.selectedCourseId = courseList[indexPath.row].courseid
+            self.selectedCourseName = courseList[indexPath.row].displayname
         }
-        searchBar.endEditing(true)
-        searchBar.text = ""
-        searching = false
-        tableView.reloadData()
-        searchBar.setShowsCancelButton(false, animated: true)
         
-
+        
+        
+        
+        
         
         performSegue(withIdentifier: "goToCourseContent", sender: self)
         
     }
     
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText == ""{
-            searching = false
-        }else{
-            searching = true
-            
-            
-            filteredCourseList = courseList.filter(){$0.displayname.contains(searchText.uppercased())}
-            
-        }
-        tableView.reloadData()
-    }
+    
+    
+    
+    
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.text = ""
         searching = false
-        tableView.reloadData()
-        searchBar.setShowsCancelButton(false, animated: true)
-        
+        searchBar.text = ""
         searchBar.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+        tableView.reloadData()
+        
     }
-    
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.setShowsCancelButton(true, animated: true)
-        searching = true
         
     }
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.endEditing(true)
-        if let cancelButton = searchBar.value(forKey: "cancelButton") as? UIButton{
-            cancelButton.isEnabled = true
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searching = true
+        filteredCourseList = courseList.filter(){$0.displayname.contains(searchText.uppercased())}
+        
+        if searchText == ""{
+            searching = false
         }
+        tableView.reloadData()
+        
     }
+    
+    
+    
+    
 }
