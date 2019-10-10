f0861e2c0a255b08b0799113bb9abbd20ee9a653
Hridik Punukollu <saihridik@gmail.com>
2019-10-10 15:57:11 +0530

    Offline devices now don't attempt to download files and instead present the user with an error. Saved files however, can still be viewed while offline.

--- CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
+++ CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
@@ -15,32 +15,6 @@ class ModuleViewController : UIViewController {
     
     var selectedModule = Module()
     
-    func setDescription(){
-        if selectedModule.moduleDescription != "" {
-        do {
-            print(selectedModule.moduleDescription)
-            
-            let formattedString = try NSAttributedString(data: ("<font size=\"+2\">\(selectedModule.moduleDescription)</font>").data(using: String.Encoding.unicode, allowLossyConversion: true)!, options: [ .documentType : NSAttributedString.DocumentType.html], documentAttributes: nil)
-            var attributedStringName = [NSAttributedString.Key : Any]()
-            if #available(iOS 13.0, *) {
-                attributedStringName = [.foregroundColor: UIColor.label]
-            }else{
-                attributedStringName = [.foregroundColor: UIColor.black]
-
-            }
-            let string = NSMutableAttributedString(attributedString: formattedString)
-            string.addAttributes(attributedStringName, range: NSRange(location: 0, length: formattedString.length))
-            descriptionText.attributedText = string
-        } catch let error {
-            print("There was an error parsing HTML: \(error)")
-        }
-        
-        descriptionText.isEditable = false
-        } else {
-            self.textConstraint.constant = 0
-        }
-    }
-    
     @IBOutlet weak var descriptionText: UITextView!
     @IBOutlet weak var textConstraint: NSLayoutConstraint!
     @IBOutlet weak var attachmentButton: UIButton!
@@ -57,9 +31,34 @@ class ModuleViewController : UIViewController {
         setDescription()
     }
     
-    
     override func viewWillDisappear(_ animated: Bool) {
         SVProgressHUD.dismiss()
+    }
+    
+    func setDescription(){
+        if selectedModule.moduleDescription != "" {
+            do {
+                print(selectedModule.moduleDescription)
+                
+                let formattedString = try NSAttributedString(data: ("<font size=\"+2\">\(selectedModule.moduleDescription)</font>").data(using: String.Encoding.unicode, allowLossyConversion: true)!, options: [ .documentType : NSAttributedString.DocumentType.html], documentAttributes: nil)
+                var attributedStringName = [NSAttributedString.Key : Any]()
+                if #available(iOS 13.0, *) {
+                    attributedStringName = [.foregroundColor: UIColor.label]
+                }else{
+                    attributedStringName = [.foregroundColor: UIColor.black]
+                    
+                }
+                let string = NSMutableAttributedString(attributedString: formattedString)
+                string.addAttributes(attributedStringName, range: NSRange(location: 0, length: formattedString.length))
+                descriptionText.attributedText = string
+            } catch let error {
+                print("There was an error parsing HTML: \(error)")
+            }
+            
+            descriptionText.isEditable = false
+        } else {
+            self.textConstraint.constant = 0
+        }
     }
     
     func saveFileToStorage(mime: String, downloadUrl: String, module: Module) {
@@ -85,19 +84,26 @@ class ModuleViewController : UIViewController {
             }
             self.navigationController?.pushViewController(docVC, animated: true)
         } else {
-            download(url: url, to: destination) {
-                SVProgressHUD.dismiss()
-                DispatchQueue.main.async {
-                    let viewURL = destination as URL
-                    let data = try! Data(contentsOf: viewURL)
-                    let webView = UIWebView(frame: self.view.frame)
-                    webView.load(data, mimeType: self.selectedModule.mimetype, textEncodingName: "", baseURL: viewURL.deletingLastPathComponent())
-                    webView.scalesPageToFit = true
-                    let docVC = UIViewController()
-                    docVC.view.addSubview(webView)
-                    docVC.title = self.selectedModule.name
-                    self.navigationController?.pushViewController(docVC, animated: true)
+            if Reachability.isConnectedToNetwork() {
+                download(url: url, to: destination) {
+                    SVProgressHUD.dismiss()
+                    DispatchQueue.main.async {
+                        let viewURL = destination as URL
+                        let data = try! Data(contentsOf: viewURL)
+                        let webView = UIWebView(frame: self.view.frame)
+                        webView.load(data, mimeType: self.selectedModule.mimetype, textEncodingName: "", baseURL: viewURL.deletingLastPathComponent())
+                        webView.scalesPageToFit = true
+                        let docVC = UIViewController()
+                        docVC.view.addSubview(webView)
+                        docVC.title = self.selectedModule.name
+                        self.navigationController?.pushViewController(docVC, animated: true)
+                    }
                 }
+            } else {
+                let alert = UIAlertController(title: "Unable to download", message: "The file cannot be downloaded as the device is offline.", preferredStyle: .alert)
+                let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
             }
         }
     }
