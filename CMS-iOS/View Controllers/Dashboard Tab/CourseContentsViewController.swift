2e48fdfe6f4fa09596fc2efbab0d78b88ea947ff
Hridik Punukollu <saihridik@gmail.com>
2019-10-16 16:17:16 +0530

    Removed stray spaces and print statements

--- CMS-iOS/View Controllers/Dashboard Tab/CourseContentsViewController.swift
+++ CMS-iOS/View Controllers/Dashboard Tab/CourseContentsViewController.swift
@@ -69,8 +69,6 @@ class CourseDetailsViewController : UITableViewController {
                     if realmSections.count != 0{
                         try! self.realm.write {
                             self.realm.delete(realmSections)
-                            // deleted
-                            print("deleted preexisting objects")
                         }
                     }
                     
@@ -79,8 +77,6 @@ class CourseDetailsViewController : UITableViewController {
                         if courseContent[i]["modules"].count > 0 {
                             let section = CourseSection()
                             section.name = courseContent[i]["name"].string!
-                            print("module count is \(courseContent[i]["modules"].count)")
-                            
                             for j in 0 ..< courseContent[i]["modules"].array!.count {
                                 let moduleData = Module()
                                 moduleData.modname = courseContent[i]["modules"][j]["modname"].string!
@@ -90,13 +86,11 @@ class CourseDetailsViewController : UITableViewController {
                                         moduleData.fileurl = (courseContent[i]["modules"][j]["contents"][0]["fileurl"].string! +
                                             "&token=\(KeychainWrapper.standard.string(forKey: "userPassword")!)")
                                         moduleData.mimetype = courseContent[i]["modules"][j]["contents"][0]["mimetype"].string!
-                                        print(moduleData.mimetype)
                                         moduleData.filename = courseContent[i]["modules"][j]["contents"][0]["filename"].string!
                                     }
                                     else {
                                         moduleData.fileurl = (courseContent[i]["modules"][j]["contents"][0]["fileurl"].string!)
                                     }
-                                    print("The url of the resource is: \(moduleData.fileurl)")
                                 } else if moduleData.modname == "forum" {
                                     moduleData.id = courseContent[i]["modules"][j]["instance"].int!
                                 }else if moduleData.modname == "folder"{
@@ -115,7 +109,6 @@ class CourseDetailsViewController : UITableViewController {
                                     }
                                 } else if moduleData.modname == "url" {
                                     moduleData.fileurl = (courseContent[i]["modules"][j]["contents"][0]["fileurl"].string!)
-                                    print("The url of the resource is: \(moduleData.fileurl)")
                                 }
                                 
                                 moduleData.name = courseContent[i]["modules"][j]["name"].string!
@@ -125,18 +118,15 @@ class CourseDetailsViewController : UITableViewController {
                                 moduleData.coursename = self.currentCourse.displayname
                                 section.modules.append(moduleData)
                                 section.courseId = self.currentCourse.courseid
-                                print(moduleData.name)
                             }
                             self.sectionArray.append(section)
                             try! self.realm.write {
                                 self.realm.add(section)
-                                print("Added section to realm")
                             }
                         }
                     }
                 }
                 completion(self.sectionArray)
-                print("function complete")
             }
         }
         else{
@@ -148,7 +138,6 @@ class CourseDetailsViewController : UITableViewController {
                 sectionArray.removeAll()
                 for i in 0..<sections.count{
                     sectionArray.append(sections[i])
-                    print("Added to section array")
                 }
             }
             updateUI()
@@ -183,7 +172,6 @@ class CourseDetailsViewController : UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("Read?: \(sectionArray[indexPath.section].modules[indexPath.row].read)")
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "reuseCourse")
         cell.textLabel?.text = sectionArray[indexPath.section].modules[indexPath.row].name
         if !sectionArray[indexPath.section].modules[indexPath.row].read {
@@ -222,7 +210,6 @@ class CourseDetailsViewController : UITableViewController {
                 cell.imageView?.image = UIImage(named: "web")
             }
         }
-        
         return cell
     }
     
