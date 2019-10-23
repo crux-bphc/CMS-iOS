0d8366396bdd93d2ebb61d8dc0d205dfd569281e
Hridik Punukollu <saihridik@gmail.com>
2019-10-23 17:44:14 +0530

    Made changes to Cells, added variables to Course object, Regex to separate course code and course name

---
+++ CMS-iOS/Support/Regex.swift
@@ -0,0 +1,25 @@
+//
+//  Regex.swift
+//  CMS-iOS
+//
+//  Created by Hridik Punukollu on 23/10/19.
+//  Copyright © 2019 Hridik Punukollu. All rights reserved.
+//
+
+import Foundation
+
+public class Regex {
+    class func match(pattern: String, text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let results = regex.matches(in: text, options: [], range: NSRange(text.startIndex..., in: text))
+            
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error{
+            print("The regex pattern was incorrect: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
