8776f1d7219b775dcc438fc7ad506e07d20c7eb9
Hridik Punukollu <saihridik@gmail.com>
2019-11-20 14:16:31 +0530

    Badge value is cleared when app is opened.
    When notifications are received and the app is opened, the badge value still stays at whatever value it was at. This commit fixes that bug.

--- CMS-iOS/AppDelegate.swift
+++ CMS-iOS/AppDelegate.swift
@@ -78,6 +78,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
