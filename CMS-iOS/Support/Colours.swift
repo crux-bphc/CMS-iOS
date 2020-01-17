a4627bef9f2ea0439a61f91f7501c77136deec15
Hridik Punukollu <saihridik@gmail.com>
2020-01-18 00:10:44 +0530

    Added separate colours file for dashboard colours

---
+++ CMS-iOS/Support/Colours.swift
@@ -0,0 +1,16 @@
+//
+//  Colours.swift
+//  CMS-iOS
+//
+//  Created by Hridik Punukollu on 17/01/20.
+//  Copyright © 2020 Hridik Punukollu. All rights reserved.
+//
+
+import UIKit
+
+class DashboardCellColours {
+    
+    let dark = [UIColor(displayP3Red: 190/255, green: 13/255, blue: 13/255, alpha: 1), UIColor(displayP3Red: 187/255, green: 81/255, blue: 0/255, alpha: 1), UIColor(displayP3Red: 153/255, green: 120/255, blue: 28/255, alpha: 1), UIColor(displayP3Red: 0/255, green: 117/255, blue: 127/255, alpha: 1), UIColor(displayP3Red: 0/255, green: 99/255, blue: 180/255, alpha: 1), UIColor(displayP3Red: 31/255, green: 63/255, blue: 137/255, alpha: 1), UIColor(displayP3Red: 109/255, green: 37/255, blue: 150/255, alpha: 1)]
+    
+    
+}
