5a057979249c8ad27050aa019dc97f0729be49fb
Aryan Chaubal <aryanchaubal@icloud.com>
2020-01-18 15:16:01 +0530

    Fixed download related issues

--- CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
+++ CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
@@ -38,9 +38,9 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
         openButton.layer.cornerRadius = 10
         quickLookController.dataSource = self
         openButton.isEnabled = true
-        if selectedModule.name != ""{
+        if selectedModule.name != "" {
             self.title = selectedModule.name
-        }else{
+        } else {
             self.title = selectedModule.filename
         }
         print(selectedModule.modname)
@@ -59,6 +59,7 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
     
     override func viewWillDisappear(_ animated: Bool) {
         task.cancel()
+        constants.downloadManager.cancelAllDownloads()
         self.progressBar.isHidden = true
         self.downloadProgressLabel.isHidden = true
         self.cancelButton.isHidden = true
@@ -140,8 +141,9 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
         } else {
             if Reachability.isConnectedToNetwork() {
                 destinationURL = destination
-//                download(url: url, to: destination)
+                //                download(url: url, to: destination)
                 downloadFile(downloadURL: url, localURL: destination) {
+                    self.locationToCopy = destination
                     self.openFile()
                 }
             } else {
@@ -158,27 +160,30 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
         generator.notificationOccurred(.success)
         openWithQL()
     }
-    func download(url: URL, to localUrl: URL) {
-        locationToCopy = localUrl
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue.main)
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        task = session.downloadTask(with: request)
-        task.resume()
-    }
+    
+    //    func download(url: URL, to localUrl: URL) {
+    //        locationToCopy = localUrl
+    //        let sessionConfig = URLSessionConfiguration.default
+    //        let session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue.main)
+    //
+    //        var request = URLRequest(url: url)
+    //        request.httpMethod = "GET"
+    //        task = session.downloadTask(with: request)
+    //        task.resume()
+    //    }        completion()
+    
     
     func downloadFile(downloadURL: URL, localURL: URL, completion: @escaping () -> Void ) {
         constants.downloadManager.showLocalNotificationOnBackgroundDownloadDone = true
         constants.downloadManager.localNotificationText = "Module completed download."
         let request = URLRequest(url: downloadURL)
-        let downloadKey = constants.downloadManager.downloadFile(withRequest: request, shouldDownloadInBackground: true, onProgress: { (progress) in
+        let downloadKey = constants.downloadManager.downloadFile(withRequest: request, shouldDownloadInBackground: false, onProgress: { (progress) in
             self.progressBar.isHidden = false
             self.downloadProgressLabel.isHidden = false
             self.progressBar.progress = Float(progress)
-            self.downloadProgressLabel.text = "Downloading: \(progress * 100)%"
+            self.downloadProgressLabel.text = "Downloading: \(Int(progress * 100))%"
             self.cancelButton.isHidden = false
+            self.cancelButton.isEnabled = true
         }) { (error, localFileURL) in
             if error != nil {
                 print("There was an error while downloading the file. \(String(describing: error))")
@@ -194,6 +199,7 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
                 } catch let removeError {
                     print("There was an error in removing: \(removeError)")
                 }
+                completion()
             }
         }
         print("The download key is: \(downloadKey ?? "")")
@@ -202,10 +208,10 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
         self.cancelButton.isEnabled = false
         self.cancelButton.isHidden = true
         self.downloadProgressLabel.isHidden = true
-        completion()
     }
     
     @IBAction func openFileButtonPressed(_ sender: UIButton) {
+        constants.downloadManager.cancelAllDownloads()
         sender.isEnabled = false
         switch selectedModule.modname {
         case "url":
@@ -248,6 +254,7 @@ class ModuleViewController : UIViewController, URLSessionDownloadDelegate, QLPre
                 self.progressBar.isHidden = false
                 self.downloadProgressLabel.isHidden = false
                 self.cancelButton.isHidden = false
+                self.cancelButton.isEnabled = true
                 
             }
             let downloadProgress = Double(totalBytesWritten) / Double(totalBytesExpectedToWrite)
