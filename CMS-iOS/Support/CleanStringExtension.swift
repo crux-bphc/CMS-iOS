db70066ea49ac1129edb364b412affece88b381b
Aryan Chaubal <aryanchaubal@icloud.com>
2020-02-19 14:12:02 +0530

    Added clean string file

--- CMS-iOS/Support/CleanStringExtension.swift
+++ CMS-iOS/Support/CleanStringExtension.swift
@@ -2,18 +2,17 @@
 //  CleanStringExtension.swift
 //  CMS-iOS
 //
-//  Created by Aryan Chaubal on 2/18/20.
+//  Created by Aryan Chaubal on 2/19/20.
 //  Copyright © 2020 Hridik Punukollu. All rights reserved.
 //
 
 import Foundation
 
 extension String {
-    
     func cleanUp() -> String {
         var clean = self
         clean = clean.replacingOccurrences(of: "&amp;", with: "&")
+        
         return clean
     }
-    
 }
