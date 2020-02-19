c5867f13a2c3e1ca8e07f8b04bb4bb9acb37bd2c
Aryan Chaubal <aryanchaubal@icloud.com>
2020-02-19 13:00:01 +0530

    fixed ios 11 download crash

--- CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
+++ CMS-iOS/View Controllers/Dashboard Tab/ModuleViewController.swift
@@ -21,7 +21,6 @@ class ModuleViewController : UIViewController, QLPreviewControllerDataSource{
     var selectedModule = Module()
     var destinationURL = URL(string: "")
     var locationToCopy = URL(string: "")
-    var task = URLSessionDownloadTask()
     let constants = Constants.Global.self
     
     @IBOutlet weak var descriptionText: UITextView!
@@ -58,7 +57,6 @@ class ModuleViewController : UIViewController, QLPreviewControllerDataSource{
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        task.cancel()
         constants.downloadManager.cancelAllDownloads()
         self.progressBar.isHidden = true
         self.downloadProgressLabel.isHidden = true
