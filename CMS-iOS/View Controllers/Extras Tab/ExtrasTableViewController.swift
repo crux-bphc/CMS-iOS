0577481c8f3a9ffa4fcd85ccd40a7ab41cd8d925
Aryan Chaubal <aryanchaubal@icloud.com>
2020-08-21 17:33:50 +0530

    Changed the extras page

--- CMS-iOS/View Controllers/Extras Tab/ExtrasTableViewController.swift
+++ CMS-iOS/View Controllers/Extras Tab/ExtrasTableViewController.swift
@@ -13,7 +13,7 @@ import SwiftKeychainWrapper
 class ExtrasTableViewController: UITableViewController {
     
     fileprivate let cellId = "ExtrasCell"
-    let items = [["T.D. Website"], ["Profile", "Logout"], ["Hide Semester On Dashboard"], ["About", "Report Bug", "Rate"]]
+    let items = [["T.D. Website"], ["Hide Semester On Dashboard"], ["About", "Report Bug", "Rate"], ["Logout"]]
     let constants = Constants.Global.self
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,15 +38,30 @@ class ExtrasTableViewController: UITableViewController {
         cell.textLabel?.text = item
         if item == "Logout" {
             cell.textLabel?.textColor = .systemRed
+            cell.imageView?.image = nil
         } else if item == "Hide Semester On Dashboard" {
             let switchView = UISwitch(frame: .zero)
             switchView.setOn(UserDefaults.standard.bool(forKey: "hidesSemester"), animated: true)
             switchView.tag = indexPath.row // for detect which row switch Changed
             switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
             cell.accessoryView = switchView
+            cell.imageView?.image = nil
+            if #available(iOS 13.0, *) {
+                cell.textLabel?.textColor = .label
+            } else {
+                // Fallback on earlier versions
+                cell.textLabel?.textColor = .black
+            }
         } else {
             cell.accessoryType = .disclosureIndicator
             cell.imageView?.image = UIImage(named: item)
+            if #available(iOS 13.0, *) {
+                cell.textLabel?.textColor = .label
+            } else {
+                // Fallback on earlier versions
+                cell.textLabel?.textColor = .black
+            }
+
         }
         return cell
 
@@ -69,29 +84,7 @@ class ExtrasTableViewController: UITableViewController {
                 break
             }
             break
-        case 1:
-            switch indexPath.row {
-            case 0:
-                // profile go to profile page
-                break
-            case 1:
-                // logout
-                let warning = UIAlertController(title: "Confirmation", message: "Are you sure you want to log out?", preferredStyle: .alert)
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                warning.addAction(cancelAction)
-                let logOutAction = UIAlertAction(title: "Yes", style: .destructive) { (_) in
-                    self.logout()
-                    self.tabBarController?.dismiss(animated: true, completion: nil)
-                    
-                }
-                warning.addAction(logOutAction)
-                self.present(warning, animated: true, completion: nil)
-                break
-            default:
-                break
-            }
-            break
-        case 3:
+        case 2:
             switch indexPath.row {
             case 0:
                 // about page
@@ -107,6 +100,25 @@ class ExtrasTableViewController: UITableViewController {
                         UIApplication.shared.open(url, options: [:], completionHandler: nil)
 
                 }
+                break
+            default:
+                break
+            }
+            break
+        case 3:
+            switch indexPath.row {
+            case 0:
+                // logout
+                let warning = UIAlertController(title: "Confirmation", message: "Are you sure you want to log out?", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                warning.addAction(cancelAction)
+                let logOutAction = UIAlertAction(title: "Yes", style: .destructive) { (_) in
+                    self.logout()
+                    self.tabBarController?.dismiss(animated: true, completion: nil)
+                    
+                }
+                warning.addAction(logOutAction)
+                self.present(warning, animated: true, completion: nil)
                 break
             default:
                 break
